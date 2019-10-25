1ce63bccba08a75b02b30901797a984cb9fc529d
Masha Ivy <mashanechmo@gmail.com>
2019-10-25 13:49:06 -0700

    finish working on branching and show the right results

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
 $(document).ready(function(){
   $("#language").submit(function(event){
+    event.preventDefault();
     var firstInput = parseInt($("input:radio[name=usability]:checked").val());
     var secondInput = parseInt($("input:radio[name=skills]:checked").val());
     var thirdInput = parseInt($("input:radio[name=characteristics]:checked").val());
-    console.log(firstInput);
-    event.preventDefault();
-  })
-})
+    var fourthInput = parseInt($("input:radio[name=name]:checked").val());
+    var total = (firstInput + secondInput + thirdInput + fourthInput);
+     console.log(total);
+     if (total <= 5){
+       $("#cresult").toggle();
+     } else if (total > 5 && total <= 9){
+       $("#javascresult").toggle()
+     } else if (total >= 12 ) {
+       $("#pythonresult").toggle()
+     }else {
+       alert("what have you done")
+     }
+  });
+});
