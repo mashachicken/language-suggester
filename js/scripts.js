14fe7a261455a0a6c991588eaf1a5ae859113b03
Masha Ivy <mashanechmo@gmail.com>
2019-10-26 13:59:15 -0700

    fix indentation

--- js/scripts.js
+++ js/scripts.js
@@ -6,21 +6,21 @@ $(document).ready(function(){
     var thirdInput = parseInt($("input:radio[name=characteristics]:checked").val());
     var fourthInput = parseInt($("input:radio[name=name]:checked").val());
     var total = (firstInput + secondInput + thirdInput + fourthInput);
-     console.log(total);
-     if (total <= 5){
-       $("#cresult").toggle();
-       $("#javascresult").hide();
-       $("#pythonresult").hide();
-     } else if (total > 5 && total <= 9){
-       $("#javascresult").toggle()
-       $("#cresult").hide()
-        $("#pythonresult").hide();
-     } else if (total >= 10 ) {
-       $("#pythonresult").toggle()
-       $("#cresult").hide();
-       $("#javascresult").hide();
-     }else {
-       alert("what have you done?!")
+    console.log(total);
+    if (total <= 5){
+      $("#cresult").toggle();
+      $("#javascresult").hide();
+      $("#pythonresult").hide();
+    }else if (total > 5 && total <= 9){
+      $("#javascresult").toggle()
+      $("#cresult").hide()
+      $("#pythonresult").hide();
+    }else if (total >= 10 ) {
+      $("#pythonresult").toggle()
+      $("#cresult").hide();
+      $("#javascresult").hide();
+    }else {
+      alert("what have you done?!")
      }
   });
 });
